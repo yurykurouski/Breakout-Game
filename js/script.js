bc169b8a0459900e5d9105aee61129bf9001f3dd
FedorowPavel <pasha_fedorow@mail.ru>
2021-01-25 17:34:18 +0200

    same comments added to code

--- js/script.js
+++ js/script.js
@@ -1,31 +1,24 @@
 import renderPage from './render/render-pages.js';
-import startTemplate from './templates/pages/initial-page.js';
-import mainTemplate from './templates/pages/main-page.js';
 import runGame from './run-game.js'
 
 
-
+//загружаем страницу при инициализации
 renderPage();
 
-
+//обработчик событий на кнопку run
+//запускаем игру
 const userNameForm = document.getElementById('run-game-form');
 userNameForm.addEventListener('submit', runGame);
 
 
-
+//обработчик события на изменения адресной строки
 window.addEventListener('popstate', () => {
-    const container = document.getElementById('container');
-    if (window.location.pathname === '/') {
-        container.innerHTML = startTemplate;
-    }
-
-    if (window.location.pathname === '/main') {
-        container.innerHTML = mainTemplate;
-    }
+    renderPage();
 })
 
 
 
+//обработчики собыйти на кнопки открыть/закрыть правила
 const rulesBtn = document.getElementById('rules-btn');
 const rulesCloseBtn = document.getElementById('close-rules-btn');
 const rules = document.getElementById('rules');

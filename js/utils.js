9521499526762a32edb370deaf7191e7c7b718ce
FedorowPavel <pasha_fedorow@mail.ru>
2021-01-25 17:36:24 +0200

    showBanner function restyled

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,4 @@
-function hideBanner(banner) {
-    banner.classList.remove('banner-show');
-}
-
 export function showBanner(banner) {
     banner.classList.add('banner-show');
-        setTimeout(hideBanner, 3000, banner);
+        setTimeout(()=>{banner.classList.remove('banner-show')}, 3000, banner);
 }

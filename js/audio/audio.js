48698458cedadcca4dd3ffaf19d38a704d799efe
FedorowPavel <pasha_fedorow@mail.ru>
2021-01-26 13:58:57 +0200

    audio files folders corrected

--- js/audio/audio.js
+++ js/audio/audio.js
@@ -11,27 +11,27 @@ export function playAudio(btn) {
         btn === rulesBtn ||
         btn === rulesCloseBtn
     ) {
-        myAudio.src = "../audio/press-buttons/rules-btn.wav";
+        myAudio.src = "../audio-files/rules-btn.wav";
         myAudio.play();
     }
 
     if 
     (btn === colorModeBtn) {
-        myAudio.src = "../audio/press-buttons/turn-light-btn.mp3";
+        myAudio.src = "../audio-files/turn-light-btn.mp3";
         myAudio.play();
     }
 
     //for errors
     if 
     (!btn) {
-        myAudio.src = "../audio/press-buttons/error.mp3";
+        myAudio.src = "../audio-files/error.mp3";
         myAudio.play();
     }
 
     //for popstate
     if 
     (btn === window) {
-        myAudio.src = "../audio/press-buttons/popstate.mp3";
+        myAudio.src = "../audio-files/popstate.mp3";
         myAudio.play();
     }
 }

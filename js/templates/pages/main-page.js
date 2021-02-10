149b9d8232074841d03c1ac2141d0f981df3c877
FedorowPavel <pasha_fedorow@mail.ru>
2021-02-10 12:08:22 +0200

    back btn on main page

--- js/templates/pages/main-page.js
+++ js/templates/pages/main-page.js
@@ -1,5 +1,6 @@
 const mainTemplate = 
 `
+
 <div class="banner banner-greeting"></div>
 <span class="payer_name"> </span>
 <span class="score">Score: </span>
@@ -11,6 +12,7 @@ const mainTemplate =
     <button id="start-game-btn" disabled>Start</button>
 </div>
 <div class="asside-buttons">
+    <button class="back-btn"><i class="fas fa-backward"></i></button>
     <button id="settings-btn" ><i class="fas fa-cog"></i></button>
     <button id="records-btn" ><i class="fas fa-trophy"></i></button>
 </div>

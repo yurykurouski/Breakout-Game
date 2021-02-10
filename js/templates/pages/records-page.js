c22d2a5c53207203bc9ce811a92520189033e02b
FedorowPavel <pasha_fedorow@mail.ru>
2021-02-10 16:52:04 +0200

    records list styled

--- js/templates/pages/records-page.js
+++ js/templates/pages/records-page.js
@@ -1,9 +1,41 @@
 const recordsTemplate = 
 `
-<h2>records</h2>
 <div class="asside-buttons">
-    <button class="back-btn"><i class="fas fa-backward"></i></button>
+        <button class="back-btn"><i class="fas fa-backward"></i></button>
 </div>
+
+<div class="records-table">
+    <div class="records">
+        <ul>
+            <li class="header">
+                <span class="index-col">Index</span>
+                <span class="record-col">Record</span>
+                <span class="player-col">Player</span>
+            </li>
+            <li>
+                <span class="index-col">1</span>
+                <span class="record-col">999</span>
+                <span class="player-col">Player</span>
+            </li>
+            <li>
+                <span class="index-col">1</span>
+                <span class="record-col">999</span>
+                <span class="player-col">Player</span>
+            </li>
+            <li>
+                <span class="index-col">1</span>
+                <span class="record-col">999</span>
+                <span class="player-col">Player</span>
+            </li>
+            <li>
+                <span class="index-col">1</span>
+                <span class="record-col">999</span>
+                <span class="player-col">Player</span>
+            </li>
+        </ul>
+    </div>
+</div>
+
 `
 
 
